862ec796d2074c599b39ca3eeafe19bc1ab93792
wcy <1101697681@qq.com>
2016-09-05 18:17:05 +0800

    dfa敏感词过滤算法

---
+++ src/main/java/com/aim/data/dfa/App.java
@@ -0,0 +1,23 @@
+package com.aim.data.dfa;
+
+import java.util.Set;
+
+import org.dom4j.DocumentFactory;
+import org.dom4j.Element;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+     
+        
+    }
+    
+    
+}
